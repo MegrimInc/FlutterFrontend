066c7ef3a87c87404e4c595be2ac912d21260ad2
Lako35 <wesleyyu1223@gmail.com>
2024-09-27 21:26:00 -0400

    Added logout button. UNTESTED

--- lib/ProfilePage/profile.dart
+++ lib/ProfilePage/profile.dart
@@ -1,3 +1,5 @@
+import 'package:barzzy_app1/AuthPages/RegisterPages/logincache.dart';
+import 'package:barzzy_app1/AuthPages/components/toggle.dart';
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatefulWidget {
@@ -8,11 +10,34 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  // Add your clearSharedPrefs function here
+  void clearSharedPrefs() {
+    final loginData = LoginCache();
+    loginData.setEmail("");
+    loginData.setPW("");
+    loginData.setSignedIn(false);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.black,
-      body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: []),
+      body: const Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [],
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.red, // Logout button color
+        onPressed: () {
+          clearSharedPrefs(); // Call the function when logging out
+          Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(builder: (context) => const LoginOrRegisterPage()), // Assuming you have this page elsewhere
+            (Route<dynamic> route) => false, // Remove all previous routes
+          );
+        },
+        child: const Icon(Icons.exit_to_app, color: Colors.white), // Icon for logout
+      ),
     );
   }
 }
