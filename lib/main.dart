843af91c4db50a8268648f7645d68d6228fcd34b
Chide <barzzy.llc@gmail.com>
2025-07-23 03:02:45 -0400

    changed the versioning to 4.0.0 we are live gang

--- lib/main.dart
+++ lib/main.dart
@@ -32,8 +32,8 @@ import 'config.dart';
 
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
-const String currentIOSMobileVersion = '3.0.0'; // Define your app version here
-const String currentIOSTabletVersion = '3.0.0'; // Define tablet version if different
+const String currentIOSMobileVersion = '4.0.0'; // Define your app version here
+const String currentIOSTabletVersion = '4.0.0'; // Define tablet version if different
 
 
 Future<void> main() async {
